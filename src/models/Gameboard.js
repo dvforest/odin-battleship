c1bf0b944702d8825982ce81e5e8f2528ace8eaa
dvforest <dvforest@hotmail.com>
2026-03-12 21:24:52 -0400

    feat(gameboard): implement basic ship placement and validation

--- src/models/Gameboard.js
+++ src/models/Gameboard.js
@@ -1,7 +1,10 @@
+import { Ship } from './Ship.js';
+
 class Gameboard {
     constructor(width, height) {
         this.width = width;
         this.height = height;
+        this.ships = [];
         this.coordinates = Array(width * height).fill(null);
     }
 
@@ -11,6 +14,25 @@ class Gameboard {
 
     assign(value, [x, y]) {
         this.coordinates[this.getIndex([x, y])] = value;
+    }
+
+    canPlaceShip(coordinates) {
+        return coordinates.every((coord) => {
+            const index = this.getIndex(coord);
+            return this.coordinates[index] === null;
+        });
+    }
+
+    placeShip(coordinates) {
+        if (!this.canPlaceShip(coordinates)) {
+            throw new Error('Invalid ship placement');
+        }
+        const ship = new Ship(coordinates.length);
+        this.ships.push(ship);
+        coordinates.forEach((coord) => {
+            this.assign(ship, coord);
+        });
+        return ship;
     }
 }
 
