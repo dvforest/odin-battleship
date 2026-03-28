5264b774ae2af33acebd71a73a027f24ca0d6e4c
dvforest <dvforest@hotmail.com>
2026-03-27 21:39:40 -0400

    feat(DOMRenderer): add basic ship rendering
    Generate separate divs for each of the board's ships, aligned onto the css grid.

--- src/ui/DOMRenderer.js
+++ src/ui/DOMRenderer.js
@@ -8,16 +8,38 @@ class DOMRenderer {
             const [x, y] = board.getCoord(index);
             const cell = createEl('div', {
                 classes: ['cell'],
-                attrs: { 'data-x': x, 'data-y': y },
+                attrs: {
+                    'data-x': x,
+                    'data-y': y,
+                    style: `grid-area: ${y + 1} / ${x + 1}`,
+                },
             });
             cells.push(cell);
         });
 
-        // Create grid layer and append cells
+        // Create ships
+        const ships = [];
+        board.ships.forEach((ship) => {
+            const xStart = ship.position[1] + 1, // Add 1 to conform to css grid 1-based indexing
+                yStart = ship.position[0] + 1;
+
+            const rStart = yStart,
+                cStart = xStart,
+                rEnd = ship.direction === 'vertical' ? yStart + ship.length : yStart,
+                cEnd = ship.direction === 'horizontal' ? xStart + ship.length : xStart;
+
+            const shipEl = createEl('div', {
+                classes: ['ship'],
+                attrs: { style: `grid-area : ${rStart} / ${cStart} / ${rEnd} / ${cEnd}` },
+            });
+            ships.push(shipEl);
+        });
+
+        // Create grid layer
         const gridLayer = createEl('div', {
             classes: ['layer', 'grid'],
             attrs: { style: `--cols:${board.width};` },
-            children: cells,
+            children: [...cells, ...ships],
         });
 
         // Create grid stage and append grid layer
